668dc3ca0fbab10281330223349a5ebed272b845
dinesh karki <dineshkrk24@gmail.com>
2020-03-27 22:55:44 +0545

    Hide menu when scrolling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi libero
+        amet nam expedita laboriosam quos odio doloremque, dolor accusantium
+        eaque, suscipit natus culpa commodi alias deserunt quod, distinctio
+        deleniti nulla delectus officia velit dolore ea voluptas aperiam!
+        Consequuntur, dolores eveniet rerum perferendis nemo ipsum quod deserunt
+        voluptas. Numquam quaerat libero quos magnam repellat aperiam sit
+        blanditiis fugiat? Dolor quasi nostrum illum ipsa blanditiis non ullam,
+        accusamus fugiat ut unde! Quidem ipsa quos enim quia vel ab dolore autem
+        ut veritatis corrupti distinctio dicta iste ad consequatur, accusantium,
+        dolor recusandae maiores quae necessitatibus, pariatur veniam. Harum
+        doloribus ipsam natus, necessitatibus dignissimos quos quo quae, placeat
+        error vel eos laborum facere, quasi iste esse hic suscipit quidem
+        voluptates temporibus. In quos veritatis consequatur, fugiat dolorum
+        velit nam corporis accusantium sunt assumenda magni quo cupiditate aut
+        aperiam placeat distinctio! Voluptates earum porro quidem accusamus
+        veritatis dignissimos iusto fugit in ad minima atque temporibus officia,
+        eligendi expedita, pariatur laborum maiores quisquam! Voluptatum, quia
+        nulla ipsum dolor corrupti reiciendis natus quasi deleniti eius fuga
+        vero quam voluptas totam inventore aliquam! Eaque necessitatibus tempora
+        corrupti distinctio voluptas illo quas eveniet tempore, explicabo
+        ratione cum! Illum qui facilis provident autem ipsum, assumenda sint
+        exercitationem dicta delectus suscipit.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi libero
+        amet nam expedita laboriosam quos odio doloremque, dolor accusantium
+        eaque, suscipit natus culpa commodi alias deserunt quod, distinctio
+        deleniti nulla delectus officia velit dolore ea voluptas aperiam!
+        Consequuntur, dolores eveniet rerum perferendis nemo ipsum quod deserunt
+        voluptas. Numquam quaerat libero quos magnam repellat aperiam sit
+        blanditiis fugiat? Dolor quasi nostrum illum ipsa blanditiis non ullam,
+        accusamus fugiat ut unde! Quidem ipsa quos enim quia vel ab dolore autem
+        ut veritatis corrupti distinctio dicta iste ad consequatur, accusantium,
+        dolor recusandae maiores quae necessitatibus, pariatur veniam. Harum
+        doloribus ipsam natus, necessitatibus dignissimos quos quo quae, placeat
+        error vel eos laborum facere, quasi iste esse hic suscipit quidem
+        voluptates temporibus. In quos veritatis consequatur, fugiat dolorum
+        velit nam corporis accusantium sunt assumenda magni quo cupiditate aut
+        aperiam placeat distinctio! Voluptates earum porro quidem accusamus
+        veritatis dignissimos iusto fugit in ad minima atque temporibus officia,
+        eligendi expedita, pariatur laborum maiores quisquam! Voluptatum, quia
+        nulla ipsum dolor corrupti reiciendis natus quasi deleniti eius fuga
+        vero quam voluptas totam inventore aliquam! Eaque necessitatibus tempora
+        corrupti distinctio voluptas illo quas eveniet tempore, explicabo
+        ratione cum! Illum qui facilis provident autem ipsum, assumenda sint
+        exercitationem dicta delectus suscipit.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi libero
+        amet nam expedita laboriosam quos odio doloremque, dolor accusantium
+        eaque, suscipit natus culpa commodi alias deserunt quod, distinctio
+        deleniti nulla delectus officia velit dolore ea voluptas aperiam!
+        Consequuntur, dolores eveniet rerum perferendis nemo ipsum quod deserunt
+        voluptas. Numquam quaerat libero quos magnam repellat aperiam sit
+        blanditiis fugiat? Dolor quasi nostrum illum ipsa blanditiis non ullam,
+        accusamus fugiat ut unde! Quidem ipsa quos enim quia vel ab dolore autem
+        ut veritatis corrupti distinctio dicta iste ad consequatur, accusantium,
+        dolor recusandae maiores quae necessitatibus, pariatur veniam. Harum
+        doloribus ipsam natus, necessitatibus dignissimos quos quo quae, placeat
+        error vel eos laborum facere, quasi iste esse hic suscipit quidem
+        voluptates temporibus. In quos veritatis consequatur, fugiat dolorum
+        velit nam corporis accusantium sunt assumenda magni quo cupiditate aut
+        aperiam placeat distinctio! Voluptates earum porro quidem accusamus
+        veritatis dignissimos iusto fugit in ad minima atque temporibus officia,
+        eligendi expedita, pariatur laborum maiores quisquam! Voluptatum, quia
+        nulla ipsum dolor corrupti reiciendis natus quasi deleniti eius fuga
+        vero quam voluptas totam inventore aliquam! Eaque necessitatibus tempora
+        corrupti distinctio voluptas illo quas eveniet tempore, explicabo
+        ratione cum! Illum qui facilis provident autem ipsum, assumenda sint
+        exercitationem dicta delectus suscipit.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi libero
+        amet nam expedita laboriosam quos odio doloremque, dolor accusantium
+        eaque, suscipit natus culpa commodi alias deserunt quod, distinctio
+        deleniti nulla delectus officia velit dolore ea voluptas aperiam!
+        Consequuntur, dolores eveniet rerum perferendis nemo ipsum quod deserunt
+        voluptas. Numquam quaerat libero quos magnam repellat aperiam sit
+        blanditiis fugiat? Dolor quasi nostrum illum ipsa blanditiis non ullam,
+        accusamus fugiat ut unde! Quidem ipsa quos enim quia vel ab dolore autem
+        ut veritatis corrupti distinctio dicta iste ad consequatur, accusantium,
+        dolor recusandae maiores quae necessitatibus, pariatur veniam. Harum
+        doloribus ipsam natus, necessitatibus dignissimos quos quo quae, placeat
+        error vel eos laborum facere, quasi iste esse hic suscipit quidem
+        voluptates temporibus. In quos veritatis consequatur, fugiat dolorum
+        velit nam corporis accusantium sunt assumenda magni quo cupiditate aut
+        aperiam placeat distinctio! Voluptates earum porro quidem accusamus
+        veritatis dignissimos iusto fugit in ad minima atque temporibus officia,
+        eligendi expedita, pariatur laborum maiores quisquam! Voluptatum, quia
+        nulla ipsum dolor corrupti reiciendis natus quasi deleniti eius fuga
+        vero quam voluptas totam inventore aliquam! Eaque necessitatibus tempora
+        corrupti distinctio voluptas illo quas eveniet tempore, explicabo
+        ratione cum! Illum qui facilis provident autem ipsum, assumenda sint
+        exercitationem dicta delectus suscipit.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nisi libero
+        amet nam expedita laboriosam quos odio doloremque, dolor accusantium
+        eaque, suscipit natus culpa commodi alias deserunt quod, distinctio
+        deleniti nulla delectus officia velit dolore ea voluptas aperiam!
+        Consequuntur, dolores eveniet rerum perferendis nemo ipsum quod deserunt
+        voluptas. Numquam quaerat libero quos magnam repellat aperiam sit
+        blanditiis fugiat? Dolor quasi nostrum illum ipsa blanditiis non ullam,
+        accusamus fugiat ut unde! Quidem ipsa quos enim quia vel ab dolore autem
+        ut veritatis corrupti distinctio dicta iste ad consequatur, accusantium,
+        dolor recusandae maiores quae necessitatibus, pariatur veniam. Harum
+        doloribus ipsam natus, necessitatibus dignissimos quos quo quae, placeat
+        error vel eos laborum facere, quasi iste esse hic suscipit quidem
+        voluptates temporibus. In quos veritatis consequatur, fugiat dolorum
+        velit nam corporis accusantium sunt assumenda magni quo cupiditate aut
+        aperiam placeat distinctio! Voluptates earum porro quidem accusamus
+        veritatis dignissimos iusto fugit in ad minima atque temporibus officia,
+        eligendi expedita, pariatur laborum maiores quisquam! Voluptatum, quia
+        nulla ipsum dolor corrupti reiciendis natus quasi deleniti eius fuga
+        vero quam voluptas totam inventore aliquam! Eaque necessitatibus tempora
+        corrupti distinctio voluptas illo quas eveniet tempore, explicabo
+        ratione cum! Illum qui facilis provident autem ipsum, assumenda sint
+        exercitationem dicta delectus suscipit.
+      </p>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default class Navbar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: true,
+      scrollPos: 0
+    };
+  }
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+  handleScroll = () => {
+    // console.log(document.body.getBoundingClientRect());
+    this.setState({
+      scrollPos: document.body.getBoundingClientRect().top,
+      show: document.body.getBoundingClientRect().top > this.state.scrollPos
+    });
+  };
+  render() {
+    console.log(this.state);
+    return <nav className={this.state.show ? "active" : "hidden"}>Navbar</nav>;
+  }
+}
